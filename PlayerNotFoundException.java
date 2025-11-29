50c39962171f69b7115c5bf91ebfae114c620067
Harshith <harshiharshith665@gmail.com>
2025-11-29 11:48:38 +0530

    PlayerNotFoundException.java

--- PlayerNotFoundException.java
+++ PlayerNotFoundException.java
@@ -1,5 +1,6 @@
 package com.codegnan.cricket.exceptions;
 
+// Custom exception thrown when the requested player is not found in the system.
 
 public class PlayerNotFoundException extends Exception {
 
